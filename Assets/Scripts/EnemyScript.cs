ae748706513041b1aed77f6764964156ae78a76c
ByronPhung <ByronPhung@users.noreply.github.com>
2015-02-21 02:56:40 -0800

    Added enemy shots.

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+	private WeaponScript weapon;
+
+	void Awake() {
+		// Retrieve the weapon only once.
+		weapon = GetComponent<WeaponScript> ();
+	}
+
+	void Update() {
+		// Auto-Fire
+		if (weapon != null && weapon.CanAttack) {
+			weapon.Attack (true);
+		}
+	}
+}
