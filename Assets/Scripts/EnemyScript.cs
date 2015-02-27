60d66c4d564a91fda989af44d3a124e449e7a159
Ara Malayan <aramalayan@gmail.com>
2015-02-27 12:49:30 -0800

    Unfinished Puffer Fish prefab
    Added incomplete version of puffer fish.  It has a flashing animation
    on hit.  It still needs some extra work.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class EnemyScript : MonoBehaviour {
 	private bool hasSpawn;
---
+++ Assets/Scripts/PufferHealth.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PufferHealth : MonoBehaviour {
+	// Define HP.
+	public int hp = 5;
+	private int hits = 0;
+	private Animator animator;
+	public Sprite spriterenderer;
+	// Identify enemy or player.
+	public bool isEnemy = true;
+	void Start()
+	{
+		animator = this.GetComponent<Animator> ();
+	}
+	
+	// Inflict damage and check if the object should be destroyed.
+	public void Damage(int damageCount) {
+		hp -= damageCount;
+		
+		if (hp <= 0) {
+			// Explosion!
+			SpecialEffectsHelper.Instance.Explosion(transform.position);
+			SoundEffectsHelper.Instance.MakeExplosionSound();
+			
+			// Dead!
+			Destroy(gameObject);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D otherCollider) {
+		// Is this a shot?
+		ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript> ();
+		
+		if (shot != null) {
+			// Avoid friendly fire.
+			if (shot.isEnemyShot != isEnemy) {
+				Damage(shot.damage);
+				hits++;
+				animator.SetBool("Collision", true);
+				animator.SetInteger ("NumHits", hits);
+				this.GetComponent<SpriteRenderer>().sprite = (spriterenderer);
+				// Destroy the shot.
+				Destroy(shot.gameObject); // Remeber to always target the game object. Otherwise, you will just remove the script.
+			}
+		}
+	}
+}
