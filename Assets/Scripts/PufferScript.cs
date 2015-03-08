0b15020495f66ace4f9bf104f4ccab6269ad9306
Ara Malayan <aramalayan@gmail.com>
2015-03-07 17:50:42 -0800

    Added PufferWeapon with tracking shot
    PufferWeapon with tracking shot added.

---
+++ Assets/Scripts/PufferScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PufferScript : MonoBehaviour
+{
+    private bool hasSpawn;
+    private MoveScript moveScript;
+    private PufferWeaponScript[] weapons;
+
+    void Awake()
+    {
+        // Retrieve the weapon only once.
+        weapons = GetComponentsInChildren<PufferWeaponScript>();
+        moveScript = GetComponent<MoveScript>();
+    }
+
+    void Start()
+    {
+        hasSpawn = false;
+        collider2D.enabled = false;
+        moveScript.enabled = false;
+
+        foreach (PufferWeaponScript weapon in weapons)
+        {
+            weapon.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        // Spawn the game object if it hasn't been spawned yet.
+        if (hasSpawn == false)
+        {
+            Spawn();
+        }
+
+        else
+        {
+            foreach (PufferWeaponScript weapon in weapons)
+            {
+                // Auto-Fire
+                if (weapon != null && weapon.CanAttack && renderer.IsVisibleFrom(Camera.main))
+                {
+                    weapon.Attack(true);
+                    SoundEffectsHelper.Instance.MakeEnemyShotSound();
+                }
+            }
+
+            // Only destroy the game object if it's less than -10.
+            // 0 is still within a quarter of the camera view so -10 is a safe number.
+            if (gameObject.transform.position.x < -10)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void Spawn()
+    {
+        hasSpawn = true;
+        collider2D.enabled = true;
+        moveScript.enabled = true;
+        foreach (PufferWeaponScript weapon in weapons)
+        {
+            weapon.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PufferWeaponScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PufferWeaponScript : MonoBehaviour
+{
+
+    public Transform shotPrefab;
+
+    public float shootingRate = 2.0f;
+
+    public Transform player;
+
+    private float shootCooldown;
+
+    void start()
+    {
+        shootCooldown = 0f;
+    }
+
+    void Update()
+    {
+        if (shootCooldown > 0)
+        {
+            shootCooldown -= Time.deltaTime;
+        }
+    }
+
+    public void Attack(bool isEnemy)
+    {
+        if (CanAttack)
+        {
+            shootCooldown = shootingRate;
+
+            var shotTransform = Instantiate(shotPrefab) as Transform;
+
+            shotTransform.position = transform.position;
+
+            ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+
+            if (shot != null)
+            {
+                shot.isEnemyShot = isEnemy;
+            }
+
+            MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
+
+            if (move != null)
+            {
+//                move.direction = player.transform.position.normalized;
+                move.direction = new Vector2(-(this.transform.position.x - player.transform.position.x), -(this.transform.position.y - player.transform.position.y)).normalized;
+            }
+        }
+    }
+
+    public bool CanAttack
+    {
+        get
+        {
+            return shootCooldown <= 0f;
+        }
+    }
+}
