7b816e6c659425609748cbd166f2be9b921a8934
Ara Malayan <aramalayan@gmail.com>
2015-03-10 01:21:19 -0700

    Create test end level script and change input
    Input buttons changed to left and right shift to avoid conflict with
    system commands.

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel : MonoBehaviour
+{
+    public string nextLevel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Application.LoadLevel(nextLevel);
+    }
+}
